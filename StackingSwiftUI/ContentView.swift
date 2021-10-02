8dd8c45a362fd955969660ffbb9ca86f70617acc
Clinton Davelaar <Davelaarprogramming@gmail.com>
2021-10-02 22:30:51 +0200

    Just messing around with stacks

---
+++ StackingSwiftUI/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  StackingSwiftUI
+//
+//  Created by Clinton Davelaar on 02/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color.green.edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                HStack(spacing: 12){
+                    Text("LBTA").padding(.all, 16).background(Color.purple)
+                    Spacer().frame(height: 5).background(Color.red)
+                }
+                Spacer().frame(width: 5).background(Color.blue)
+                HStack(spacing: 12){
+                    Spacer().frame(height: 5).background(Color.red)
+                    Text("LBTA").padding(.all, 16).background(Color.purple)
+                }
+            }.background(Color.yellow)
+        }
+        
+        
+//        HStack{
+//
+//            Text("LBTA")
+//            Spacer().frame(height: 5).background(Color.green)
+//            Text("LBTA")
+//        }.background(Color.yellow)
+//
+//        VStack{
+//            Text("LBTA 123").background(Color.red)
+//            Spacer().frame(width: 10).background(Color.orange)
+//            Text("LBTA").background(Color.red)
+//        }.background(Color.blue)
+//        Text("Hello, world")
+//            .background(Color.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StackingSwiftUI/StackingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackingSwiftUIApp.swift
+//  StackingSwiftUI
+//
+//  Created by Clinton Davelaar on 02/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct StackingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
